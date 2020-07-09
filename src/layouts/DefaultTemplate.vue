43034270b27474f24d6c3299b69eb992d7df68e0
leandronegreiros <leandronegreiros07@gmail.com>
2020-07-09 16:48:36 -0400

    instalacao do vueRouter

--- src/layouts/DefaultTemplate.vue
+++ src/layouts/DefaultTemplate.vue
@@ -1,6 +1,18 @@
 <template>
     <div>
-        Vue Food
+        <ul>
+            <li>
+                <router-link :to="{name: 'home'}">Home</router-link>
+            </li>
+            <li>
+                <router-link :to="{name: 'products'}">Products</router-link>
+            </li>
+            <li>
+                <router-link :to="{name: 'cart'}">Carrinho</router-link>
+            </li>
+        </ul>
+
+        <router-view></router-view>
     </div>
 </template>
 
---
+++ src/pages/Products.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        Produto
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
