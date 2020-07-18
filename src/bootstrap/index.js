4bdd54e3e478d38b5d1d240968d8bfc9da87d0f7
leandronegreiros <leandronegreiros07@gmail.com>
2020-07-18 09:27:16 -0400

    instalacao do axios

---
+++ src/bootstrap/index.js
@@ -0,0 +1,4 @@
+import axios from 'axios'
+import { URI_BASE_APE } from '../configs/api'
+
+axios.defaults.baseURL = URI_BASE_APE
---
+++ src/configs/api.js
@@ -0,0 +1,3 @@
+export const URI_BASE_APE = 'http://larafood.test/api/'
+export const API_VERSION = 'V1'
+export  const TOKEN_NAME = 'token_sanctum'
